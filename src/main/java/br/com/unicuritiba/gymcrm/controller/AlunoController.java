918ff41d5eba0591061db8e9a1a129748b55457f
palma1607 <palma1607@gmail.com>
2026-09-23 20:10:45 -0300

    Update AlunoController.java

--- src/main/java/br/com/unicuritiba/gymcrm/controller/AlunoController.java
+++ src/main/java/br/com/unicuritiba/gymcrm/controller/AlunoController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +25,34 @@ public class AlunoController {
 	public ResponseEntity<List<Aluno>> getAlunos() {
 		
 		List<Aluno> alunos = repositorio.findAll();
-		
 		return ResponseEntity.ok(alunos);
+	}
+	
+	@GetMapping("/alunos/{id}")
+	public ResponseEntity<Aluno> getAlunoById(
+			@PathVariable long id){
+		
+		Aluno aluno = repositorio.getReferenceById(id);
+		return ResponseEntity.ok(aluno);
+	}
+	
+	@DeleteMapping("/alunos/{id}")
+	public ResponseEntity<Aluno> deleteAlunoById(
+			@PathVariable long id){
+		
+		repositorio.deleteById(id);
+		return ResponseEntity.ok(null);
+	}
+	
+	@PutMapping("/alunos/{id}")
+	public ResponseEntity<Aluno> updateAlunoById(
+			@PathVariable long id,
+			@RequestBody Aluno aluno
+			){
+		
+		aluno.setId(id);
+		Aluno updatedAluno = repositorio.save(aluno);
+		return ResponseEntity.ok(updatedAluno);
 	}
 	
 	@PostMapping("/alunos")
@@ -31,7 +60,6 @@ public class AlunoController {
 			@RequestBody Aluno aluno){
 		
 		Aluno alunoSalvo = repositorio.save(aluno);
-		
 		return ResponseEntity.ok(alunoSalvo);
 	}
 	
